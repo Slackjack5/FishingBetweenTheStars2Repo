f635117b6f0b968da54719014e0f82bcd5091dc8
Nycelly <dkr264@nyu.edu>
2022-01-07 17:08:22 -0500

    Inventory System Pretty Much Finished

--- FishingBetweenTheStars2Unity/Assets/Project/Scripts/PlayerPDA/InventoryTab.cs
+++ FishingBetweenTheStars2Unity/Assets/Project/Scripts/PlayerPDA/InventoryTab.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 using UnityEngine.UI;
+using TMPro;
 
 public class InventoryTab : UdonSharpBehaviour
 {
@@ -13,15 +14,7 @@ public class InventoryTab : UdonSharpBehaviour
   public FishDictionary fishDictionary;
   public GameObject slotObjectSelected;
   private int slotIdSelected;
-
-  private void FixedUpdate()
-  {
-    if (fishDictionary.getFishData(0)!=null) 
-    {
-      AddToBag(6);
-    }
-
-  }
+  public Sprite unknownFish;
 
   public void AddToBag(int Id)
   {
@@ -34,7 +27,14 @@ public class InventoryTab : UdonSharpBehaviour
         //Put Fish Id in that Slot
         FishId[i] = Id;
         //Change Sprite in that Slot
-        Child.GetComponent<Image>().sprite = fishDictionary.getFishData(FishId[i]).getFishSprite();
+        if(fishDictionary.getFishData(FishId[i]).getFishSprite()!=null)
+        {
+          Child.GetComponent<Image>().sprite = fishDictionary.getFishData(FishId[i]).getFishSprite();
+        }
+        else
+        {
+          Child.GetComponent<Image>().sprite = unknownFish;
+        }
         //Say the Slot is Full
         isFull[i] = true;
         break;
@@ -58,13 +58,52 @@ public class InventoryTab : UdonSharpBehaviour
 
   public void UpdateMainPanel(int Id)
   {
+    //Change Main Image
+
     //Get The Child Image of our Slot
     GameObject Child = inventorySlots[inventorySlots.Length-1].transform.GetChild(0).gameObject;
     //Put Fish Id in that Slot
     FishId[inventorySlots.Length-1] = Id;
     //Change Sprite in that Slot
-    Child.GetComponent<Image>().sprite = fishDictionary.getFishData(FishId[inventorySlots.Length-1]).getFishSprite();
+    if(fishDictionary.getFishData(FishId[inventorySlots.Length - 1]).getFishSprite()!=null)
+    {
+      Child.GetComponent<Image>().sprite = fishDictionary.getFishData(FishId[inventorySlots.Length - 1]).getFishSprite();
+    }
+    else
+    {
+      Child.GetComponent<Image>().sprite = unknownFish;
+    }
+
     //Say the Slot is Full
+   
     isFull[inventorySlots.Length-1] = true;
+    
+   //Change Fish Name 
+   Child = inventorySlots[inventorySlots.Length - 1].transform.GetChild(1).gameObject;
+   Child.GetComponent<TMPro.TextMeshProUGUI>().text = fishDictionary.getFishData(FishId[inventorySlots.Length - 1]).getFishName();
+   //Change Fish Description
+   Child = inventorySlots[inventorySlots.Length - 1].transform.GetChild(2).gameObject;
+   Child.GetComponent<TMPro.TextMeshProUGUI>().text = fishDictionary.getFishData(FishId[inventorySlots.Length - 1]).getFishDescription();
+   //Change Value Number
+   Child = inventorySlots[inventorySlots.Length - 1].transform.GetChild(3).gameObject;
+   Child.GetComponent<TMPro.TextMeshProUGUI>().text = fishDictionary.getFishData(FishId[inventorySlots.Length - 1]).getFishValue().ToString();
+   //Change Power Number
+   Child = inventorySlots[inventorySlots.Length - 1].transform.GetChild(4).gameObject;
+   Child.GetComponent<TMPro.TextMeshProUGUI>().text = fishDictionary.getFishData(FishId[inventorySlots.Length - 1]).getFishPower().ToString();
+   //Change Favorite Food Image
+   Child = inventorySlots[inventorySlots.Length - 1].transform.GetChild(5).gameObject;
+   if (fishDictionary.getFishData(FishId[inventorySlots.Length - 1]).getFavoriteFish()==9999)
+   {
+     Child.GetComponent<Image>().sprite = unknownFish;
+   }
+   else
+   {
+     Child.GetComponent<Image>().sprite = fishDictionary.getFishData(fishDictionary.getFishData(FishId[inventorySlots.Length - 1]).getFavoriteFish()).getFishSprite();
+   }
+   //Change Tier Number
+   Child = inventorySlots[inventorySlots.Length - 1].transform.GetChild(6).gameObject;
+   Child.GetComponent<TMPro.TextMeshProUGUI>().text = fishDictionary.getFishData(FishId[inventorySlots.Length - 1]).getFishTier().ToString();
+   
+   
   }
 }
